4e52b3d91fd6e2335a6ea3c2042dd8738f9d78cd
jaredbsanchez-png <326318537+jaredbsanchez-png@users.noreply.github.com>
2026-09-13 12:57:50 -0700

    apps-ipod: add playback and playlist API shims for USB iAP

---
+++ apps-ipod/api/playback.h
@@ -0,0 +1,7 @@
+/***************************************************************************
+ * GNU General Public License (version 2+)
+ *
+ * Boundary shim. USB iAP firmware includes "playback.h" by bare name;
+ * the real PodBox header is audio/playback.h.
+ ****************************************************************************/
+#include "../audio/playback.h"
---
+++ apps-ipod/api/playlist.h
@@ -0,0 +1,7 @@
+/***************************************************************************
+ * GNU General Public License (version 2+)
+ *
+ * Boundary shim. USB iAP firmware includes "playlist.h" by bare name;
+ * the real PodBox header is playlist/playlist.h.
+ ****************************************************************************/
+#include "../playlist/playlist.h"
